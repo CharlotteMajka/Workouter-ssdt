9a28328c42bacc9806777314ea1b1bd5c75ddeac
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2021-03-12 13:42:17 +0100

    Update WorkoutTypes.sql

--- db/Workouter-SSDT/WorkoutTypes.sql
+++ db/Workouter-SSDT/WorkoutTypes.sql
@@ -3,5 +3,3 @@ CREATE TABLE [dbo].[WorkoutTypes]
   [Id] INT NOT NULL PRIMARY KEY,
   [Name] NVARCHAR(100)
 );
-INSERT INTO [dbo].[WorkoutTypes] (Name)
-VALUES ('Running'), ('Swimming'),('Walking');
