e0cb13351ea0917469f21f0ce55f0478475f4485
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2021-03-18 12:56:39 +0100

    Update insert.sql

--- db/Workouter-SSDT/insert.sql
+++ db/Workouter-SSDT/insert.sql
@@ -1,3 +1,3 @@
 -- Write your own SQL object definition here, and it'll be included in your package.
 INSERT INTO [dbo].[WorkoutTypes] (Name)
-VALUES ('Running'), ('Swimming'),('Walking');
+VALUES ('Running');
